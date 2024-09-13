68f243aeb37967a4cd1854cf7afa06dc04f648bd
Matheus F. V. Pinto <matheus.fvp@outlook.com>
2024-09-12 22:21:54 -0300

    feat: database configuration

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
-from src import create_app
+from src import create_app, db
+from flask import current_app
+from src.models.user_model import User
 
 app = create_app()
+
+@app.cli.command("create-db")
+def create_db():
+    """Comando para criar o banco de dados"""
+    with current_app.app_context():
+        db.create_all()
+    print("Banco de dados criado com sucesso!")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/__init__.py
+++ src/__init__.py
@@ -5,6 +5,8 @@ from src.db import db
 def create_app(config_class='config.DevelopmentConfig'):
     app = Flask(__name__)
     app.config.from_object(config_class)
-    register_routes(app)
+    
     db.init_app(app)
+
+    register_routes(app)
     return app
